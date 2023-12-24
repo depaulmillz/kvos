7c4e2b6d428496f0ea89d4770e6f88d44a2360d4
dePaul Miller <4567dep@gmail.com>
2023-12-24 12:14:17 -0500

    Updated setup

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@ rustup override set nightly
 rustup target add x86_64-unknown-none
 rustup component add rust-src
 
-sudo apt-get install -y nasm grub-common grub-pc-bin xorriso qemu
+sudo apt-get install -y nasm grub-common grub-pc-bin xorriso qemu qemu-utils
 
 ./create_disks.sh
